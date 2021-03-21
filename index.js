c4bbd73f4a4fde8a6e312413129b21c874217ae8
Master-DOK <master7dok@gmail.com>
2021-03-21 17:27:03 +0100

    add directory and files

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express'
-
+import path from 'path'
+const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Express</h1>')
+   // res.send('<h1>Hello Express</h1>')
+    res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 })
+app.get('/features', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'static', 'features.html'))
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server has been running on port ${PORT}...`)
